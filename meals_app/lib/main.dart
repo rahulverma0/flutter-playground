c6cb30a1f3f9afcdbdea82eaa293ffdfc32ae661
RV <rahulverma05@yahoo.com>
2020-05-09 05:50:09 -0400

    meal-app

---
+++ meals_app/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import './screens/filters_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/mean_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RonotoCondensed',
+                fontWeight: FontWeight.bold,
+              ))),
+      home: CategoriesScreen(),
+      //initialRoute: '/',//defaul is '/'
+      routes: {
+        '/':(ctx)=>TabsScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routerName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName:(ctx)=>FiltersScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        //return MaterialPageRoute(builder: (ctx)=>CategoriesScreen(),);
+      },
+      /*
+      This is used for 404 etc */
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+    );
+  }
+}
---
+++ meals_app/lib/screens/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../widgets/category_item.dart';
+import '../dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  GridView(
+        padding: EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: <Widget>[
+          ...DUMMY_CATEGORIES
+              .map(
+                (catData) => CategoryItem(
+                  catData.id,
+                  catData.title,
+                  catData.color,
+                ),
+              )
+              .toList(),
+        ],
+      );
+  }
+}
